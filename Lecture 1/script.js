1c80888926e8f83945fdcc0d42316811fbd360a3
Tushar Bansal <tusharbansal069@gmail.com>
2023-12-31 20:50:17 +0530

    Restructured folder

--- Lecture 1/script.js
+++
@@ -1,6 +0,0 @@
-const root = document.getElementById('root');
-
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello Everyone From JavaScript";
-
-root.appendChild(heading);
--- Lecture 2/dist/index.672d4772.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", {}, "Hello Everyone From React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.672d4772.js.map
