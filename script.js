6e4190dcffec9eb95c330422163a1bb40593a998
Tushar Bansal <tusharbansal069@gmail.com>
2024-01-01 22:23:18 +0530

    Episode 3

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// JSX (transpiled before it reaches the JS)
 
-const heading = React.createElement("h1",{},"Hello Everyone From React");
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// JSX => React.createElement => React Element (JS Object) => HTML Element(render)
 
-root.render(heading) 
+const jsxHeading = (
+  <h1 id="heading" tabIndex="5">
+    Namaste Everyone from JSX!
+  </h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
